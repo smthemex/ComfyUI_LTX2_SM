2f49a3aaad857cdfdc087238c7ebfe5d8b0ab225
smthemex <138738845+smthemex@users.noreply.github.com>
2026-03-07 09:49:24 +0800

    2.3

--- LTX2/ltx_core/conditioning/types/__init__.py
+++ LTX2/ltx_core/conditioning/types/__init__.py
@@ -1,9 +1,13 @@
 """Conditioning type implementations."""
 
+from .attention_strength_wrapper import ConditioningItemAttentionStrengthWrapper
 from .keyframe_cond import VideoConditionByKeyframeIndex
 from .latent_cond import VideoConditionByLatentIndex
+from .reference_video_cond import VideoConditionByReferenceLatent
 
 __all__ = [
+    "ConditioningItemAttentionStrengthWrapper",
     "VideoConditionByKeyframeIndex",
     "VideoConditionByLatentIndex",
+    "VideoConditionByReferenceLatent",
 ]
